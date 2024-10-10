370fd7afcc71c7fdba2a6fade45bce0833eb396a
teha <tehaabdurahman@gmail.com>
2024-10-10 12:12:10 -0700

    update nvmrc

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "dist",
+   build: {
+    outDir: 'dist',
     sourcemap: false,
-  },
-});
+  }
+})
