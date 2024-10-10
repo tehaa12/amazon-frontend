e2c73e7703d6ee75c460b30069f8984e0cb0f031
teha <tehaabdurahman@gmail.com>
2024-10-10 16:58:48 -0700

    update the npm

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-   build: {
-    outDir: 'dist',
+  build: {
+    outDir: "dist",
     sourcemap: false,
-  }
-})
+    rollupOptions: {
+      external: ["react-router-dom"],
+    },
+  },
+});
