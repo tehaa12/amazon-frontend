66a34aebdcd6e20870300618e2a7271ea6a70e0a
teha <tehaabdurahman@gmail.com>
2024-11-10 00:06:44 -0800

    Fix module resolution for react-responsive-carousel

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ["react-responsive-carousel"],
+  },
   build: {
     chunkSizeWarningLimit: 1000, // Adjust as needed
     rollupOptions: {
