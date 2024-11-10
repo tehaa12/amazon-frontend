d5c7263f5f77573b8bd480865388a864c6d09abd
teha <tehaabdurahman@gmail.com>
2024-11-09 21:42:12 -0800

    Fix import path for Carousel component

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Carousel from "../../components/carousel/carousel";
+import Carousel from "../../components/carousel/Carousel"
 import Catagory from "../../components/catagory/Catagory";
 import Layout from "../../Layout/Layout";
 import Product from "../../components/Products/Product";
