aa4ffccce9efe0f46fab0d5a5365218b518642c1
teha <tehaabdurahman@gmail.com>
2024-11-09 22:08:48 -0800

    Add react-responsive-carousel as external to Rollup config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1000, // Adjust as needed
+    rollupOptions: {
+      external: ["react-responsive-carousel"], // Exclude react-responsive-carousel
+    },
   },
 });
