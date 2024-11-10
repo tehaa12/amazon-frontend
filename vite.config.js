9ad255408231fe88fb7d1f48df021141466db77c
teha <tehaabdurahman@gmail.com>
2024-11-10 00:25:04 -0800

    Fix react-responsive-carousel configuration

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,5 @@ export default defineConfig({
   },
   build: {
     chunkSizeWarningLimit: 1000, // Adjust as needed
-    rollupOptions: {
-      external: [
-        "react-responsive-carousel",
-        "react-responsive-carousel/lib/styles/carousel.min.css", // Externalize the CSS file as well
-      ],
-    },
   },
 });
